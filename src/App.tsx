3b55f896aafd157b68f4b4e5e99ac2112f30614b
Fred Silva <fredsilva015@gmail.com>
2021-11-28 20:08:20 +0100

    added some styles and elements to APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './styles/App.css';
 import { Form } from './components';
+import { useSelector } from 'react-redux';
+import { Puff } from 'svg-loaders-react';
+import { Box } from '@material-ui/system';
+import type { RootState } from 'redux/store';
 
 function App(): JSX.Element {
+  const mainData = useSelector((state: RootState) => state.data.value);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <Box
+          sx={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {mainData.length && mainData ? <Form /> : <Puff stroke="#98ff98" />}
+        </Box>
       </header>
     </div>
   );
