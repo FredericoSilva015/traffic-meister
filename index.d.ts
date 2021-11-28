679e411da5399cde283d5804b43f48006a2ee038
Fred Silva <fredsilva015@gmail.com>
2021-11-28 20:07:55 +0100

    added module and package

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare module 'svg-loaders-react';
+
+// Not sure why it isn't picking up yet
+// declare module 'test' {
+//   import trafficMeister from 'service';
+//   import type { VehicleTypes } from 'type';
+//   export const trafficMeister: fetchData<() => VehicleTypes[] | error> =
+//     trafficMeister;
+// }
